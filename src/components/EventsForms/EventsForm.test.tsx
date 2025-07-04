938ab3320358b5a2f2236f0b1f4b8810e0585a46
Vishnu Prasad <vishnu.msvp@gmail.com>
2025-07-04 01:35:34 +0100

    feat(update): more test coverage for event form

--- src/components/EventsForms/EventsForm.test.tsx
+++ src/components/EventsForms/EventsForm.test.tsx
@@ -1,6 +1,10 @@
-import { screen, render, fireEvent } from "@testing-library/react";
+import { screen, render, fireEvent, waitFor } from "@testing-library/react";
 import { describe, expect, it, vi } from "vitest";
 import EventsForm from ".";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
 const mockPositions = {
   ID: "1",
@@ -17,19 +21,32 @@ const mockPositions = {
   ],
 };
 
-const mockMutateFn = vi.fn().mockImplementation(() => ({
-    success: true,
-    Positions: mockPositions,
-  }));
+const mockMutateFn = vi.fn().mockImplementation(
+  async () =>
+    await Promise.resolve({
+      isSuccess: true,
+      isError: false,
+      isLoading: false,
+      isIdle: false,
+      data: { Positions: mockPositions },
+    })
+);
+vi.mock("@tanstack/react-query", async () => {
+  const originalModule = await vi.importActual("@tanstack/react-query");
+  return {
+    ...originalModule,
+    useMutation: vi.fn().mockImplementation(() => ({
+      mutate: mockMutateFn,
+      data: { Positions: [] },
+      isSuccess: false,
+      isError: false,
+      isLoading: false,
+      isIdle: true,
+    })),
+  };
+});
 
 describe("EventsForm Component Test Suites", () => {
-  
-  vi.mock("@tanstack/react-query", () => ({
-    useMutation: () => ({
-      mutate: mockMutateFn,
-    }),
-  }));
-
   it("should render the form with correct title and button text", () => {
     render(
       <EventsForm title="Test Event Form" buttonText="Submit" pageType="Buy" />
@@ -41,18 +58,25 @@ describe("EventsForm Component Test Suites", () => {
 
   it("should be able to submit the form with test data", () => {
     render(
-      <EventsForm title="Test Event Form" buttonText="Submit" pageType="Buy" />
+      <QueryClientProvider client={new QueryClient()}>
+        <EventsForm
+          title="Test Event Form"
+          buttonText="Submit"
+          pageType="Buy"
+        />
+      </QueryClientProvider>
     );
     const accountInput = screen.getByPlaceholderText("Account");
     const securityInput = screen.getByPlaceholderText("Security");
     const quantityInput = screen.getByPlaceholderText("Quantity");
     const submitButton = screen.getByTestId("submit-button");
-
     fireEvent.change(accountInput, { target: { value: "Test Account" } });
     fireEvent.change(securityInput, { target: { value: "Test Security" } });
     fireEvent.change(quantityInput, { target: { value: "100" } });
     fireEvent.click(submitButton);
-    expect(mockMutateFn).toHaveBeenCalledTimes(0);
+    waitFor(() => {
+      expect(mockMutateFn).toBeCalledTimes(1);
+    });
   });
   it("should add a security when the add button is clicked", () => {
     render(
@@ -61,7 +85,6 @@ describe("EventsForm Component Test Suites", () => {
 
     const addButton = screen.getByTestId("add-security-button");
     fireEvent.click(addButton);
-
     expect(screen.getAllByRole("textbox").length).toBe(6); // One for account and one for security
   });
   it("should remove a security when the remove button is clicked", () => {
